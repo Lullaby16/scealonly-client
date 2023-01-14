058af7d2c06398ab03e70b06770d4e043537e767
Lullaby16 <samuelys945@gmail.com>
2023-01-14 13:03:00 +0700

    final v.1

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    origin: "localhost",
-    port: 3000,
-  },
+  // server: {
+  //   origin: "localhost",
+  //   port: 3000,
+  // },
 });
